60973d3a7bc0e8d76874bc7decddb493a0818070
ManalEzzat <manalezzat590@gmail.com>
2025-10-19 22:13:48 +0300

    Update PlayerAudio.h

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -1,1 +1,22 @@
-
+#include <JuceHeader.h>;
+class PlayerAudio{
+public:
+MainComponent();
+~MainComponent() override;
+ void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+ void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+ void releaseResources() override;
+ bool LoadFile(const juce::File file) ;
+ void play();
+ void stop();
+ void setGain(float gain);
+ void setPosition(double pos);
+ double getLength() const;
+ double geyPosition() const;
+private:
+    
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+ JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+};
