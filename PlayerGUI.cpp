5279aa1bcd27fd17b29910e261d30cd6fa428d4b
Rawan20240197 <20240197@stud.fci-cu.edu.eg>
2025-10-21 13:48:24 +0300

    Initialize PlayerGUI with buttons

--- PlayerGUI.cpp
+++ PlayerGUI.cpp
@@ -1,1 +1,52 @@
+# include "PlayerGUI.h"
 
+PlayerGUI::PlayerGUI() {
+
+
+	addAndMakeVisible(pausebutton);
+	pausebutton.setButtonText("pause||");
+	addAndMakeVisible(playbutton);
+	playbutton.setButtonText("play ►");
+	
+	
+	addAndMakeVisible(gotostart);
+	gotostart.setButtonText("go to start|◄");
+	addAndMakeVisible(end);
+	end.setButtonText("end ►|");
+	pausebutton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
+	pausebutton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
+	pausebutton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+
+
+
+	playbutton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
+	playbutton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
+	playbutton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+
+
+	gotostart.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
+	gotostart.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
+	gotostart.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+
+
+	end.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
+	end.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
+	end.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+
+	setSize(700, 400);
+
+
+}
+PlayerGUI::~PlayerGUI(){}
+void PlayerGUI::paint(juce::Graphics& interface) {
+	interface.fillAll(juce::Colours::black);
+}
+
+void PlayerGUI::resized() {
+	pausebutton.setBounds(50, 50, 100, 60);
+	playbutton.setBounds(160, 50, 100, 60);
+	
+	gotostart.setBounds(380, 50, 100, 60);
+	end.setBounds(490, 50, 100, 60);
+
+}
