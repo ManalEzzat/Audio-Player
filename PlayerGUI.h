26fb7709b33297f10d400ecf8d562b63999080a2
Rawan20240197 <20240197@stud.fci-cu.edu.eg>
2025-10-21 13:49:16 +0300

    Add PlayerGUI class with button

--- PlayerGUI.h
+++ PlayerGUI.h
@@ -1,1 +1,19 @@
+# pragma once 
+# include<JuceHeader.h>
 
+class PlayerGUI : public juce::Component {  
+
+public:
+	PlayerGUI(); 
+	~PlayerGUI();  
+
+	void paint(juce::Graphics&) override;
+	void resized() override;
+
+private:
+	juce::TextButton playbutton;
+	juce::TextButton pausebutton;
+	juce::TextButton gotostart;
+	juce::TextButton end;
+
+};
