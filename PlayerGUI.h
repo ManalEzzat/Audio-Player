50af563fe61baeb6daccad09066b052e9220854e
Rawan20240197 <20240197@stud.fci-cu.edu.eg>
2025-10-22 21:22:59 +0300

    update PlayerGUI.h
    Updated PlayerGUI class to include buttonClicked listener and added new buttons.

--- PlayerGUI.h
+++ PlayerGUI.h
@@ -1,19 +1,23 @@
-# pragma once 
-# include<JuceHeader.h>
+#pragma once
+#include <JuceHeader.h>
+#include "PlayerAudio.h"
 
-class PlayerGUI : public juce::Component {  
-
+class PlayerGUI : public juce::Component, public juce::Button::Listener {
 public:
-	PlayerGUI(); 
-	~PlayerGUI();  
+    PlayerGUI();
+    ~PlayerGUI();
 
-	void paint(juce::Graphics&) override;
-	void resized() override;
+    void paint(juce::Graphics&) override;
+    void resized() override;
+    void buttonClicked(juce::Button* button) override;
 
 private:
-	juce::TextButton playbutton;
-	juce::TextButton pausebutton;
-	juce::TextButton gotostart;
-	juce::TextButton end;
+    juce::TextButton loadbutton{ "Load" };
+    juce::TextButton playbutton{ "play ►" };
+    juce::TextButton stopbutton{ "pause ||" };
+    juce::TextButton gotostart{ "go to start|◄" };
+    juce::TextButton end{ "end ►|" };
 
+    PlayerAudio player; 
+    std::unique_ptr<juce::FileChooser> fileChooser;
 };
