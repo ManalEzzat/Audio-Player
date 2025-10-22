20a2c950cfe9e9ccfa2bc64784b3bfb960235c66
Rawan20240197 <20240197@stud.fci-cu.edu.eg>
2025-10-22 21:24:00 +0300

    Implement basic audio player functionality

--- PlayerAudio.cpp
+++ PlayerAudio.cpp
@@ -1,1 +1,24 @@
+# include "PlayerAudio.h"
+# include <iostream>
+using namespace std;
+PlayerAudio::PlayerAudio() {
 
+	formataudio.registerBasicFormats();
+
+}
+PlayerAudio:: ~PlayerAudio() {}
+
+
+
+void PlayerAudio::play() {
+	transportSource.start(); // play
+}
+void PlayerAudio::pause() {
+	transportSource.stop(); // pause 
+}
+void PlayerAudio::gotostart() {
+	transportSource.setPosition(0.0);// go to start 
+}
+void PlayerAudio::end() {
+	transportSource.setPosition(transportSource.getLengthInSeconds()); // it ends
+}
