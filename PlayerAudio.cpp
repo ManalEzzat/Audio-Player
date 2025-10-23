bf2a8554663a58602b97ec1cb88dfde95a5e2657
ManalEzzat <manalezzat590@gmail.com>
2025-10-23 12:44:37 +0300

    Update PlayerAudio.cpp

--- PlayerAudio.cpp
+++ PlayerAudio.cpp
@@ -8,17 +8,34 @@ PlayerAudio::PlayerAudio() {
 }
 PlayerAudio:: ~PlayerAudio() {}
 
+bool PlayerAudio::loadFile(const juce::File& file) {
+	if (auti* reader = formatManger.creatReaderFor(file)) {
+        transportSource.stop();
+        transportSource.setSource(nullptr);
+        readerSource.reset();
 
+        readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+
+        transportSource.setSource(readerSource.get(),0,nullptr, reader->sampleRate);
+        return true;
+    }
+    return false; 
+}
 
 void PlayerAudio::play() {
-	transportSource.start(); // play
+	transportSource.start(); 
 }
 void PlayerAudio::pause() {
-	transportSource.stop(); // pause 
+	transportSource.stop(); 
 }
 void PlayerAudio::gotostart() {
-	transportSource.setPosition(0.0);// go to start 
+	transportSource.setPosition(0.0);
 }
 void PlayerAudio::end() {
-	transportSource.setPosition(transportSource.getLengthInSeconds()); // it ends
+	transportSource.setPosition(transportSource.getLengthInSeconds());
+}
+void PlayerAudio::setLooping(bool loop)
+{
+    if (readerSource != nullptr)
+        readerSource->setLooping(loop);
 }
