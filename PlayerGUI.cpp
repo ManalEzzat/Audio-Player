2731948b04708c64514ff4222d5b8a01de7af4a7
ManalEzzat <manalezzat590@gmail.com>
2025-10-28 18:18:41 +0300

    Update PlayerGUI.cpp

--- PlayerGUI.cpp
+++ PlayerGUI.cpp
@@ -1,94 +1,87 @@
-# include "PlayerGUI.h"
+#include "PlayerGUI.h"
 
-PlayerGUI::PlayerGUI() {
+PlayerGUI::PlayerGUI()
+{
+   
+    addAndMakeVisible(playButton);
+    playButton.setButtonText("Play ►");
+    playButton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
+    playButton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
+    playButton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+    playButton.addListener(this);
 
+   
+    addAndMakeVisible(pauseButton);
+    pauseButton.setButtonText("Pause ||");
+    pauseButton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
+    pauseButton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
+    pauseButton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+    pauseButton.addListener(this);
 
-	addAndMakeVisible(pausebutton);
-	pausebutton.setButtonText("pause||");
-	addAndMakeVisible(playbutton);
-	playbutton.setButtonText("play ►");
+    
+    addAndMakeVisible(gotoStartButton);
+    gotoStartButton.setButtonText("|◄ Start");
+    gotoStartButton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
+    gotoStartButton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
+    gotoStartButton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+    gotoStartButton.addListener(this);
 
+    
+    addAndMakeVisible(endButton);
+    endButton.setButtonText("End ►|");
+    endButton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
+    endButton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
+    endButton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+    endButton.addListener(this);
 
-	addAndMakeVisible(gotostart);
-	gotostart.setButtonText("go to start|◄");
-	addAndMakeVisible(end);
-	end.setButtonText("end ►|");
-	pausebutton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
-	pausebutton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
-	pausebutton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+    
+    addAndMakeVisible(loopButton);
+    loopButton.setButtonText("Loop 🔁");
+    loopButton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
+    loopButton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
+    loopButton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
+    loopButton.addListener(this);
 
-
-
-	playbutton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
-	playbutton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
-	playbutton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
-
-
-	gotostart.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
-	gotostart.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
-	gotostart.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
-
-
-	end.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
-	end.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
-	end.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
-
-	addAndMakeVisible(loopButton);
-	loopButton.setButtonText("Loop 🔁");
-	loopButton.setColour(juce::TextButton::buttonColourId, juce::Colours::gold);
-	loopButton.setColour(juce::TextButton::textColourOffId, juce::Colours::black);
-	loopButton.setColour(juce::TextButton::textColourOnId, juce::Colours::red);
-	loopButton.addListener(this);
-
-
-	setSize(700, 400);
-
-
+    setSize(700, 400);
 }
+
 PlayerGUI::~PlayerGUI() {}
-void PlayerGUI::paint(juce::Graphics& interface) {
-	interface.fillAll(juce::Colours::black);
+
+void PlayerGUI::paint(juce::Graphics& interface)
+{
+    interface.fillAll(juce::Colours::black);
 }
 
-void PlayerGUI::resized() {
-	pausebutton.setBounds(50, 50, 100, 60);
-	playbutton.setBounds(160, 50, 100, 60);
-
-	gotostart.setBounds(380, 50, 100, 60);
-	end.setBounds(490, 50, 100, 60);
-	loopButton.setBounds(600, 50, 100, 60);
-
-
+void PlayerGUI::resized()
+{
+    playButton.setBounds(50, 50, 120, 60);
+    pauseButton.setBounds(180, 50, 120, 60);
+    gotoStartButton.setBounds(310, 50, 120, 60);
+    endButton.setBounds(440, 50, 120, 60);
+    loopButton.setBounds(570, 50, 120, 60);
 }
+
 void PlayerGUI::buttonClicked(juce::Button* button)
 {
-	if (button == &loadbutton) {
-		fileChooser = std::make_unique<juce::FileChooser>(
-			"Select an audio file...", juce::File{}, "*.wav;*.mp3");
-
-		fileChooser->launchAsync(
-			juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles,
-			[this](const juce::FileChooser& chooser)
-			{
-				auto file = chooser.getResult();
-				if (file.existsAsFile())
-				{
-					player.loadFile(file);
-				}
-			});
-	}
-	else if (button == &playbutton)
-		player.play();
-	else if (button == &stopbutton)
-		player.pause();
-	else if (button == &gotostart)
-		player.gotostart();
-	else if (button == &end)
-		player.end();
-	else if (button == &loopButton)
-	{
-		isLooping = !isLooping;
-		player.setLooping(isLooping);
-	}
+    if (button == &playButton)
+    {
+        player.play();
+    }
+    else if (button == &pauseButton)
+    {
+        player.pause();
+    }
+    else if (button == &gotoStartButton)
+    {
+        player.gotostart();
+    }
+    else if (button == &endButton)
+    {
+        player.end();
+    }
+    else if (button == &loopButton) {
+        isLooping = !isLooping;
+        player.setLooping(isLooping);
+    }
 
 }
