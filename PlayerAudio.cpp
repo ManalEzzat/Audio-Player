5747ce4abc26d2869598920e6aee1a8bb97aecd3
ManalEzzat <manalezzat590@gmail.com>
2025-10-28 18:12:02 +0300

    Update PlayerAudio.cpp

--- PlayerAudio.cpp
+++ PlayerAudio.cpp
@@ -1,39 +1,51 @@
-# include "PlayerAudio.h"
-# include <iostream>
+#include "PlayerAudio.h"
+#include <iostream>
 using namespace std;
-PlayerAudio::PlayerAudio() {
 
-	formataudio.registerBasicFormats();
-
+PlayerAudio::PlayerAudio()
+{
+    formatAudio.registerBasicFormats();
 }
-PlayerAudio:: ~PlayerAudio() {}
 
-bool PlayerAudio::loadFile(const juce::File& file) {
-	if (auti* reader = formatManger.creatReaderFor(file)) {
+PlayerAudio::~PlayerAudio() {}
+
+bool PlayerAudio::loadFile(const juce::File& file)
+{
+    if (auto* reader = formatAudio.createReaderFor(file))
+    {
         transportSource.stop();
         transportSource.setSource(nullptr);
         readerSource.reset();
 
         readerSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
 
-        transportSource.setSource(readerSource.get(),0,nullptr, reader->sampleRate);
+        transportSource.setSource(readerSource.get(), 0, nullptr, reader->sampleRate);
+
         return true;
     }
-    return false; 
+    return false;
 }
 
-void PlayerAudio::play() {
-	transportSource.start(); 
+void PlayerAudio::play()
+{
+    transportSource.start();
 }
-void PlayerAudio::pause() {
-	transportSource.stop(); 
+
+void PlayerAudio::pause()
+{
+    transportSource.stop();
 }
-void PlayerAudio::gotostart() {
-	transportSource.setPosition(0.0);
+
+void PlayerAudio::gotostart()
+{
+    transportSource.setPosition(0.0);
+
 }
-void PlayerAudio::end() {
-	transportSource.setPosition(transportSource.getLengthInSeconds());
+void PlayerAudio::end()
+{
+    transportSource.setPosition(transportSource.getLengthInSeconds());
 }
+
 void PlayerAudio::setLooping(bool loop)
 {
     if (readerSource != nullptr)
