a68dd8b97493b3b7dd7db15745699ef35c4b2d08
Rawan20240197 <20240197@stud.fci-cu.edu.eg>
2025-11-03 20:35:24 +0200

    Refactor PlayerGUI class and add ListBoxModel

--- PlayerGUI.h
+++ PlayerGUI.h
@@ -1,28 +1,50 @@
-#pragma once
-#include <JuceHeader.h>
-#include "PlayerAudio.h"
+# pragma once
+# include <JuceHeader.h>
+# include "PlayerAudio.h"
 
-class PlayerGUI : public juce::Component,
-    public juce::Button::Listener
+class PlayerGUI :public juce::Component,
+    public juce::Button::Listener,
+    public juce::ListBoxModel
+
 {
 public:
     PlayerGUI();
-    ~PlayerGUI() override;
+    ~PlayerGUI();
 
-    void paint(juce::Graphics&) override;
     void resized() override;
+    void paint(juce::Graphics& g);
+  
+    PlayerAudio& getPlayer() { return player; }
+
     void buttonClicked(juce::Button* button) override;
 
-private:
-    PlayerAudio player;
+    int getNumRows() override;
+    void paintListBoxItem(int numaudios, juce::Graphics& g, int width, int height, bool audioselected);
+    void selectedRowsChanged(int audioselected);
 
+private:
+
+    std::unique_ptr<juce::FileChooser> fileChooser;
+
+    juce::TextButton loadButton;
     juce::TextButton playButton;
     juce::TextButton pauseButton;
-    juce::TextButton gotoStartButton;
-    juce::TextButton endButton;
     juce::TextButton loopButton;
+    juce::TextButton goToStartButton;
+    juce::TextButton endButton;
 
-    bool isLooping = false;
+    juce::ListBox audiolistbox; // to add playlist gui
 
+    juce::TextButton muteButton;
+
+    juce::Label titlelabel; // to add title in gui title
+    juce::Label authorlabel; // to add the text in gui author 
+    juce::Label durationlabel; // to add duration in gui duration
+
+    
+    juce::Array<juce::File> audiolist;
+    PlayerAudio player;  
+    int audioindex = -1;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
+
 };
