8e4788e8441d0942f9b96aa3f6d9ad995166ba0b
Rawan20240197 <20240197@stud.fci-cu.edu.eg>
2025-11-03 20:34:10 +0200

    Refactor PlayerAudio class and add new methods
    Refactor PlayerAudio class to improve structure and add new methods.

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -1,23 +1,44 @@
 #pragma once
-#include <JuceHeader.h>
+# include <JuceHeader.h>
 
-class PlayerAudio
+class PlayerAudio :public juce::Component
 {
 public:
-    PlayerAudio();
-    ~PlayerAudio();
+	PlayerAudio();
+	~PlayerAudio();
 
-    bool loadFile(const juce::File& file);
-    void play();
-    void pause();
-    void gotostart();
-    void end();
-    void setLooping(bool loop);
+	void prepareToPlay(int samplesPerBlockExpected, double sampleRate) ;
+	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) ;
+	void releaseResources() ;
+	void loadFile(const juce::File& file);
+
+
+
+	void play();
+	void pause();
+	void restart();
+	void end();
+	
+
+	std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+	
+	
 
 private:
-    juce::AudioFormatManager formatAudio;
-    juce::AudioTransportSource transportSource;
-    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+	juce::AudioFormatManager formatManager;
+	juce::AudioTransportSource transportSource;
+	
+	std::unique_ptr<juce::FileChooser> fileChooser;
+	
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
-};
+	float previousVolume = 1.0f;
+	
+
+	
+
+	juce::Label titleLabel;
+	juce::Label authorLabel;
+	juce::Label durationLabel;
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio);
+
+}; 
