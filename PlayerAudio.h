e4ff0551e801b0c6c3f39ac6d1dfe4da08952014
shahd6ahmed-ctrl <shahd6ahmed@gmail.com>
2025-11-05 00:04:03 +0200

    Refactor PlayerAudio to inherit from AudioSource
    Refactor PlayerAudio class to inherit from juce::AudioSource and override necessary methods. Added new member variables and removed unused ones.

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -1,51 +1,40 @@
 #pragma once
-# include <JuceHeader.h>
+#include <JuceHeader.h>
 
-class PlayerAudio :public juce::Component
+class PlayerAudio : public juce::AudioSource
 {
 public:
-	PlayerAudio();
-	~PlayerAudio();
+    PlayerAudio();
+    ~PlayerAudio() override;
 
-	void prepareToPlay(int samplesPerBlockExpected, double sampleRate) ;
-	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) ;
-	void releaseResources() ;
-	void loadFile(const juce::File& file);
+    // AudioSource interface
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    bool loadFile(const juce::File& file);
+
+    void play();
+    void pause();
+    void restart();
+    void end();
+    void setGain(float gain);
+    void setSpeed(double speed);
+    void setPosition(double pos);
+    double getPosition() const;
+    double getLength() const;
 
 
-
-	void play();
-	void pause();
-	void restart();
-	void end();
-    void setLoopStart();
-    void setLoopEnd();
-    void clearLoop();
-    void checkLoop();
-
-
-	std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
-	
-	
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
 
 private:
-	juce::AudioFormatManager formatManager;
-	juce::AudioTransportSource transportSource;
-	
-	std::unique_ptr<juce::FileChooser> fileChooser;
-	
+    juce::AudioFormatManager formatManager;
+    juce::AudioTransportSource transportSource;
+    std::unique_ptr<juce::ResamplingAudioSource> resamplingSource;  
+    std::unique_ptr<juce::FileChooser> fileChooser;
+    double currentSpeed = 1.0;
+    float previousVolume = 1.0f;
+   
 
-	float previousVolume = 1.0f;
-	
-    double loopStart = 0.0;
-    double loopEnd = 0.0;
-    bool isLoopActive = false;
-
-	
-
-	juce::Label titleLabel;
-	juce::Label authorLabel;
-	juce::Label durationLabel;
-	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio);
-
-}; 
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
+};
