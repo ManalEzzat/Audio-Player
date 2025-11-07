e341d5d13ff6eb5e0be13c7a07af9bb0d18705ec
shahd6ahmed-ctrl <shahd6ahmed@gmail.com>
2025-11-07 13:48:31 +0200

    Add looping functionality to PlayerAudio

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -21,6 +21,9 @@ public:
     void setGain(float gain);
     void setSpeed(double speed);
     void setPosition(double pos);
+    void FullLoop(bool shoudloop);
+    void SectionLoop(bool shoudloop, double start, double end);
+    void checkloop();
     double getPosition() const;
     double getLength() const;
     void tenforward();
@@ -37,6 +40,10 @@ private:
     double currentSpeed = 1.0;
     float previousVolume = 1.0f;
 
+    bool isFullLoop = false;
+    bool isSectionLoop = false;
+    double loopStart = 0.0;
+    double loopEnd = 0.0;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
 };
